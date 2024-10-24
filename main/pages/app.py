fcf42c87ba8e834dae5fc2d0f2b95ecaf25db800
lexamenrf44 <alexander.stcstudent@gmail.com>
2024-10-24 22:52:28 +0200

    Fixed app.py for playwright usage

--- main/pages/app.py
+++ main/pages/app.py
@@ -1,25 +1,24 @@
-from playwright.sync_api import expect
-from main.config import Settings
-from main.data import Data
+import time
 
-settings = Settings()
+from playwright.sync_api import expect, Page
+
+from main.support.data import Data
+
 data = Data()
 
-
 class App:
-    # Methods
-    def login_ui(self, page, username=data.user_test_login, password=data.user_test_password, success=True):
-        page.goto(settings.base_url)
-        page.fill(self.app_username, username)
-        page.fill(self.app_password, password)
-        page.click(self.app_login_button)
-        if success:
-            expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
-        else:
-            expect(page).to_have_url("https://www.saucedemo.com/")
+    def __init__(self, page: Page):
+        self.page = page
+        self.app_username = '[data-test="username"]'
+        self.app_password = '[data-test="password"]'
+        self.app_login_button = '[data-test="login-button"]'
+        self.app_logo = '.header_label'
 
-    # Common locators
-    app_username = '[data-test="username"]'
-    app_password = '[data-test="password"]'
-    app_login_button = '[data-test="login-button"]'
-    app_logo = '.header_label'
+    def login_ui(self, username=data.user_test_login, password=data.user_test_password, success=True):
+        self.page.fill(self.app_username, username)
+        self.page.fill(self.app_password, password)
+        self.page.click(self.app_login_button)
+        if success:
+            expect(self.page).to_have_url("https://www.saucedemo.com/inventory.html")
+        else:
+            expect(self.page).to_have_url("https://www.saucedemo.com/")
